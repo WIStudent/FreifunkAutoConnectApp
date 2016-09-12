f781fe3cdcb1354b54a3061ebed10ac491c52c43
Tobias Trumm <tobiastrumm@uni-muenster.de>
2016-09-12 18:55:22 +0200

    Refactor InfoActivity using Butter Knife

--- app/src/main/java/com/example/tobiastrumm/freifunkautoconnect/InfoActivity.java
+++ app/src/main/java/com/example/tobiastrumm/freifunkautoconnect/InfoActivity.java
@@ -1,25 +1,34 @@
 package com.example.tobiastrumm.freifunkautoconnect;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class InfoActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.wv_info_page) WebView wv_info_page;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ButterKnife.bind(this);
 
         // Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         // Webview
-        WebView wv_info_page = (WebView) findViewById(R.id.wv_info_page);
         wv_info_page.loadUrl(getString(R.string.info_page_url));
     }
 }
